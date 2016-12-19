0415bbf2bace9954905cc9f5801aec1ac0754100
Tatiana Limache Quispe <Tati34che@gmail.com>
2016-12-19 16:29:08 -0500

    Ejercicio 1 Terminado

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Ejercicio1
+Inicio : Suma de dos Numeros
+##Ingreso de Datos
+- Numero1
+- Numero2
+- Resultado
+
+##Proceso
+- Pedir Numero1
+- Pedir Numero2
+-Sumar Numero1 y Numero2 y almacenar en resultado
+-Mostrar Resultado
---
+++ main.js
@@ -0,0 +1,9 @@
+var numero1=null;
+var numero2=null;
+var resultado=0;
+numero1=prompt("Por favor ingresa un numero1");
+numero2=prompt("Por favor ingresa un numero2");
+resultado=numero1 + numero2 ;
+
+ resultado=(parseFloat(numero1)+parseFloat(numero2));
+  alert(resultado);
